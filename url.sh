4a3add65e4b4092c6b30a3636c979f4a6a5116cf
nagata-labot <nagata@labot.inc>
2022-06-14 10:35:31 +0000

    fix url.sh

--- url.sh
+++ url.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/sh
-ENV_DIR_PATH=~/environment
-TMP_DIR_PATH=${ENV_DIR_PATH}/codegym-scripts
-REPOSITORY_DIR_PATH=${ENV_DIR_PATH}/codegym-app-security-challenge
+
+REPOSITORY_DIR_PATH=${PWD}
 ENV_FILE_PATH=${REPOSITORY_DIR_PATH}/.env
 
 ip=`ec2-metadata -v | sed -e 's/public-ipv4: //g'`
